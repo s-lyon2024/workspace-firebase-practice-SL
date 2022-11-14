e074b3b29ac73a75a5dbec536f0de0f7695e3436
s-lyon2024 <111667797+s-lyon2024@users.noreply.github.com>
2022-11-14 12:50:04 -0500

    firebase data collection added

--- js/hotel-json.js
+++ js/hotel-json.js
@@ -1,14 +1,13 @@
 /* Change the configuration */
 
-var firebaseConfig = {
-  apiKey: 'AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM',
-  authDomain: 'grocerylist-91956.firebaseapp.com',
-  databaseURL: 'https://grocerylist-91956.firebaseio.com',
-  projectId: 'grocerylist-91956',
-  storageBucket: 'grocerylist-91956.appspot.com',
-  messagingSenderId: '813812426276',
-  appId: '1:813812426276:web:93e5897af12892ff78dab1',
-  measurementId: 'G-VZ83BTR72T',
+const firebaseConfig = {
+  apiKey: 'AIzaSyC9IFN_gsJbH-vsNw2bghWq5Shp3zMO3y8',
+  authDomain: 'grocies-700b7.firebaseapp.com',
+  projectId: 'grocies-700b7',
+  storageBucket: 'grocies-700b7.appspot.com',
+  messagingSenderId: '616117787461',
+  appId: '1:616117787461:web:8ec17f04f27cfe51fbfc2c',
+  measurementId: 'G-KVEGFW5T0Q',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -16,13 +15,22 @@ firebase.initializeApp(firebaseConfig);
 // enter data in
 $("input[type='button']").click(function (e) {
   //get the value of form
+  var inputdata = $('form').serializeArray();
+  var data = {}; // data to be sent to database
+  // console.log(inputdata[2].name);
+  // console.log(inputdata[2].value);
 
   /* save the data to database */
+  inputdata.forEach((entry) => {
+    console.log(entry);
+    data[entry.name] = entry.value;
+  });
+  console.log(data);
+  firebase.firestore().collection('hotelcollections').add(data);
 
   /* clear the entry */
   $('form')[0].reset();
 });
-
 
 /* array example
 const array1 = ['a', 'b', 'c'];
