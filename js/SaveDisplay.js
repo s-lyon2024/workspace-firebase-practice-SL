71c3844f4c04be446324064986113f1a1f6b0866
s-lyon2024 <111667797+s-lyon2024@users.noreply.github.com>
2022-11-16 13:04:51 -0500

    survey display done signup started

--- js/SaveDisplay.js
+++ js/SaveDisplay.js
@@ -1,18 +1,75 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: 'AIzaSyC9IFN_gsJbH-vsNw2bghWq5Shp3zMO3y8',
+  authDomain: 'grocies-700b7.firebaseapp.com',
+  projectId: 'grocies-700b7',
+  storageBucket: 'grocies-700b7.appspot.com',
+  messagingSenderId: '616117787461',
+  appId: '1:616117787461:web:8ec17f04f27cfe51fbfc2c',
+  measurementId: 'G-KVEGFW5T0Q',
+};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // save the data
-$(".sampleSurvey input[type='submit']").click(function(e) {
+$(".sampleSurvey input[type='submit']").click(function (e) {
   e.preventDefault();
-
   // get the value of the form using serializeArray method
-  
+  //get the value of form
+  var inputdata = $('form').serializeArray();
+  var data = {}; // data to be sent to database
 
-
-
+  /* save the data to database */
+  inputdata.forEach((entry) => {
+    // console.log(entry);
+    data[entry.name] = entry.value;
+  });
+  //console.log(data);
+  firebase.firestore().collection('surveydata').add(data);
+  /* clear the entry */
+  $('form')[0].reset();
 });
 
-// update the result in table
+/* read the data from the database */
+firebase
+  .firestore()
+  .collection('surveydata')
+  .onSnapshot((querySnapshot) => {
+    var nA = 0;
+    var nB = 0;
+    var nC = 0;
+    var nD = 0;
+    var nE = 0;
+    console.log(querySnapshot.size);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      console.log(doc.data().choice);
+      console.log(doc.data().comm);
+      // update the result in table
+      var s = doc.data().choice;
+      switch (s) {
+        case 'A':
+          nA++;
+          $('#ans1').text(nA);
+          break;
+        case 'B':
+          nB++;
+          $('#ans2').text(nB);
+          break;
+        case 'C':
+          nC++;
+          $('#ans3').text(nC);
+          break;
+        case 'D':
+          nD++;
+          $('#ans4').text(nD);
+          break;
+        case 'E':
+          nE++;
+          $('#ans5').text(nE);
+          break;
+      }
+    });
+  });
--- js/Signup.js
+++ js/Signup.js
@@ -1,36 +1,36 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM",
-  authDomain: "grocerylist-91956.firebaseapp.com",
-  databaseURL: "https://grocerylist-91956.firebaseio.com",
-  projectId: "grocerylist-91956",
-  storageBucket: "grocerylist-91956.appspot.com",
-  messagingSenderId: "813812426276",
-  appId: "1:813812426276:web:93e5897af12892ff78dab1",
-  measurementId: "G-VZ83BTR72T"
+  apiKey: 'AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM',
+  authDomain: 'grocerylist-91956.firebaseapp.com',
+  databaseURL: 'https://grocerylist-91956.firebaseio.com',
+  projectId: 'grocerylist-91956',
+  storageBucket: 'grocerylist-91956.appspot.com',
+  messagingSenderId: '813812426276',
+  appId: '1:813812426276:web:93e5897af12892ff78dab1',
+  measurementId: 'G-VZ83BTR72T',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // save the data
-$("#signup-form").submit(function(e) {
+$('#signup-form').submit(function (e) {
   e.preventDefault();
   // get the username(email) and password from the form
   // change the following code
-  var email = "yilianz4@gmail.com";
-  var password = "ddsgagafda";
+  var email = 'lyons@gmail.com';
+  var password = '12345bobbie';
 
   // create a user with email address and password
   firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
-    .then(user => {
+    .then((user) => {
       // Signed in
       // ...
 
-      console.log("You are signed up");
-      window.location.href = "Login.html";
+      console.log('You are signed up');
+      window.location.href = 'Login.html';
     })
-    .catch(error => {
+    .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
       console.log(error.code);
