521cb9e68573ecedb402ce9d08d68dc479fa6ec4
s-lyon2024 <111667797+s-lyon2024@users.noreply.github.com>
2022-11-21 13:02:44 -0500

    Google sign in working

--- js/Signin.js
+++ js/Signin.js
@@ -41,32 +41,31 @@ $('#Login').submit(function (e) {
       var errorMessage = error.message;
       console.log(errorMessage);
     });
+});
 
-  $('#signGoogle').click(function() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    console.log('Google');
-    //const auth = getAuth();
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const user = result.user;
-        console.log('Google user' + credential.email + 'Log in');
-        //  const token = credential.accessToken;
-        // The signed-in user info.
-        //  const user = result.user;
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
-  });
+$('#signGoogle').click(function () {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  //const auth = getAuth();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const user = result.user;
+      console.log('Google user' + user.email + 'Log in');
+      //  const token = credential.accessToken;
+      // The signed-in user info.
+      //  const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      //const errorCode = error.code;
+      console.log('Error message: ' + error.message);
+      // The email of the user's account used.
+      //const email = error.customData.email;
+      // The AuthCredential type that was used.
+      // const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
 });
